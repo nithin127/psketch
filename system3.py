3839b21e112c4890fbe1d67399faabcdd09ede52
vasisthn <nithin127vasisth@gmail.com>
2019-11-23 04:42:08 -0500

    Ran into error. System 3. Indexing; objects

--- system3.py
+++ system3.py
@@ -1,3 +1,4 @@
+import copy
 import pickle
 import random
 import numpy as np
@@ -47,9 +48,10 @@ number_inventory = {7: "iron", 8: "grass", 9: "wood", 10: "gold", 11: "gem", 12:
 
 
 class Node():
-	def __init__(self, inventory = None, state_space = None):
-		self.inventory = inventory
-		self.state_space = state_space
+	def __init__(self, inventory = None, state_space = None, graph = None):
+		self.inventory = inventory.copy()
+		self.state_space = state_space.copy()
+		self.graph = copy.deepcopy(graph)
 		self.skills_so_far = []
 		self.cost_so_far = 0
 		self.reward_so_far = 0
@@ -79,6 +81,7 @@ class System3():
 		self.reusable_events = []
 		self.required_inventory = np.zeros(len(self.rule_dict[random_key][1][0]))
 		self.bonus_inventory = np.zeros(len(self.rule_dict[random_key][1][0]))
+		self.graph_guide = None
 		
 
 	def reset_demo_objectives(self):
@@ -89,6 +92,7 @@ class System3():
 		self.reusable_events = []
 		self.required_inventory = np.zeros(len(self.rule_dict[random_key][1][0]))
 		self.bonus_inventory = np.zeros(len(self.rule_dict[random_key][1][0]))
+		self.graph_guide = None
 
 
 	def infer_objective(self, rule_sequence, reachability_set_sequence, event_position_sequence):
@@ -213,10 +217,12 @@ class System3():
 		for obj, event_num in inventory_condition_buffer: 
 			self.reusable_events.append(obj)
 			self.bonus_inventory[obj] += 1
-		return self.required_inventory, self.bonus_inventory
+		return {"independent_events": self.independent_events, "reusable_events": self.reusable_events, \
+				"required_inventory": self.required_inventory, "bonus_inventory": self.bonus_inventory}
 
 
 	def get_prerequisite(self, node, graph_nodes, node_count = 0):
+		# Not sure how general is the AND/OR graph
 		pre_requisite = []
 		pre_requisite_node_index = []
 		new_node_count = 0
@@ -261,6 +267,7 @@ class System3():
 
 	def get_reachability_condition(self, initial_config, goal, free_space_id = 0, obstacle_id = 2):
 		usable_objects = []
+		costs = []
 		init_x, init_y = np.where(initial_config == 1)
 		init_x, init_y = init_x[0], init_y[0]
 		world = np.clip(initial_config, free_space_id, obstacle_id)
@@ -272,88 +279,250 @@ class System3():
 		while len(to_visit) > 0:
 			curr = to_visit.pop(0)
 			for nx, ny, d in find_neighbors(curr, None):
-				if world[nx, ny] == obstacle_id:
-					obj = int(initial_config[nx, ny])
-					# Insane Hard-coding B-)
-					# Also considering the same objects multiple times. Hihi
-					if not (obj in self.unpassable_objects or obj == obstacle_id):
-						usable_objects.append((nx, ny, obj))
-					continue
 				cost = cost_map[curr[0],curr[1]] + 1
 				if cost < cost_map[nx,ny]:
+					cost_map[nx,ny] = cost
+					if world[nx, ny] == obstacle_id:
+						obj = int(initial_config[nx, ny])
+						# Insane Hard-coding B-)
+						# Also considering the same objects multiple times. Hihi
+						if not obj == obstacle_id:
+							usable_objects.append(((nx, ny), obj, curr))
+							costs.append(cost)
+						continue
+				
 					if world[nx, ny] == free_space_id:
 						to_visit.append((nx, ny))
-					cost_map[nx,ny] = cost
-		return not (cost_map[init_x, init_y] == np.inf), list(set(usable_objects))
-		
+					
+		return not (cost_map[init_x, init_y] == np.inf), usable_objects, costs
 
-	def adapt_to_env(self, new_nodes, env_adapted_graph, env_adapted_node_dependency, initial_config):
-		new_nodes = [node[0] for node in new_nodes]
-		new_keys = []
-		for obj in new_nodes:
+
+	def adapt_to_env(self, nodes, env_adapted_graph, env_adapted_node_dependency, initial_config):
+		new_nodes = []
+		for obj, _ in nodes:
 			# Get positions
 			xs, ys = np.where(initial_config == obj)
 			env_adapted_graph[obj] = [(x, y) for x, y in zip(xs, ys)]
 			# Check reachability
+			# And update graph "appropriately"... lol
 			for x, y in zip(xs, ys):
-				reachable, possible_dependencies = self.get_reachability_condition(initial_config, (x, y))
+				reachable, possible_dependencies, _ = self.get_reachability_condition(initial_config, (x, y))
 				if not reachable:
-					env_adapted_node_dependency[(x,y)] = [ (px, py) for px, py, _ in possible_dependencies]
-					for _, _, obj_d in possible_dependencies:
-						if (obj_d in new_keys) or (obj_d in new_nodes) or (obj_d in list(env_adapted_graph.keys())):
-							pass
-						else:
-							new_keys.append(obj_d)
-			# Add to graph or Add to new keys
-			# for _, _, obj in possible_dependencies:
-			#	if obj in 
-		return new_keys, env_adapted_graph, env_adapted_node_dependency
+					env_adapted_node_dependency[(x,y)] = []
+					for obj_pos , obj, _ in possible_dependencies:
+						if not obj in self.unpassable_objects:
+							rules = np.where(self.rule_dict[obj][0][:,-1] == -1)[0]
+							for rule in rules:
+								env_adapted_node_dependency[(x,y)].append((obj_pos ,(obj, rule)))
+								if ((obj, rule) in new_nodes) or ((obj, rule) in nodes) or (obj in list(env_adapted_graph.keys())):
+									pass
+								else:
+									new_nodes.append((obj, rule))
+		return new_nodes, env_adapted_graph, env_adapted_node_dependency
 
 
-	def get_dependency_graph_guide(self, initial_config, objectives, objective_type="reward"):
+	def get_dependency_graph_guide(self, initial_config):
 		# Let's form the graph skeleton: we're assuming this would not be an AND/OR graph
-		if objective_type == "event":
-			leftover_keys = objectives
-			# Core skeleton
-			graph_nodes = []
-			graph_skeleton = {}
-			# Appended skeleton
-			env_adapted_graph = {}
-			env_adapted_node_dependency = {}
-			# Iteration
-			while len(leftover_keys) > 0:
-				graph_nodes, graph_skeleton, new_nodes = self.update_core_graph_skeleton(leftover_keys, graph_nodes, graph_skeleton)
-				leftover_keys, env_adapted_graph, env_adapted_node_dependency = self.adapt_to_env(new_nodes, env_adapted_graph, env_adapted_node_dependency, initial_config)
-		elif objective_type == "reward":
-			raise("Haven't implemented")
-		else:
-			raise("Unrecognised objective type: {}".format(objective_type))
+		leftover_keys = self.independent_events + self.reusable_events
+		# Core skeleton
+		graph_nodes = []
+		graph_skeleton = {}
+		# Appended skeleton
+		env_adapted_graph = {}
+		env_adapted_node_dependency = {}
+		# Iteration
+		while len(leftover_keys) > 0:
+			graph_nodes, graph_skeleton, new_nodes = self.update_core_graph_skeleton(leftover_keys, graph_nodes, graph_skeleton)
+			leftover_keys, env_adapted_graph, env_adapted_node_dependency = self.adapt_to_env(new_nodes, env_adapted_graph, env_adapted_node_dependency, initial_config)
 		# Now let's adapt the graph skeleton to the current environment instance and fill in the details
-		return graph_skeleton, graph_nodes, env_adapted_graph, env_adapted_node_dependency
+		self.graph_guide = {"skeleton": graph_skeleton, "nodes": graph_nodes, "node_pos": env_adapted_graph, \
+							"node_pos_dependencies": env_adapted_node_dependency}
+		return self.graph_guide
 
 
-	def approximate_transition_step(self, config, skill):
+	def graph_cost(self, node, alpha=0.3, gamma=0.9):
+		# There are some repeated calculations each time we try to figure out the cost
+		# but for now, we make do with this
+		# 
+		# alpha: independent to reusable/bonus event reward ratio
+		# beta: cost/reward ratio that we'll care for
+		# gamma: same as RL. discount factor (used to propagate rewards through the graph)
+		#
+		inventory = node.inventory
+		graph = node.graph
+		inventory_reward = abs(self.required_inventory - inventory) + alpha*abs(self.bonus_inventory - inventory)
+		nodes = []
+		rewards = []
+		event__rewards = []
+		non_reachable_env_node__rewards = []
+		for obj in np.where(inventory_reward > 0)[0]:
+			rule_indices =  np.where(self.rule_dict_transitions_unwrapped[:, obj] >  0)[0]
+			rules = [self.rule_dict_unwrapped_index[rule_index] for rule_index in rule_indices]
+			for rule in rules: 
+				event_rewards.append(rule, inventory_reward[obj])
+		while (len(event__rewards) > 0) or (len(non_reachable_env_node__rewards) > 0):
+			# Going through core graph skeleton
+			if len(event__rewards) > 0:
+				event, reward = event__rewards.pop()
+				# Now we propagate reward and get nodes
+				# If inventory condition satisfied
+				if (inventory - self.rule_dict[event[0]][1][event[1]] >= 0).all():
+					# Consider the environment specific position nodes
+					env_nodes = graph["node_pos"][event[0]]
+					for env_node in env_nodes:
+						if env_node in graph["node_pos_dependencies"].keys():
+							non_reachable_env_node__rewards.append((env_node, reward*gamma))
+						else:
+							nodes.append(env_node)
+							rewards.append(reward)
+				else:
+					# add children to list :D
+					event_id = graph["nodes"].index(event)
+					for or_event_list in graph["skeleton"][event_id]:
+						for or_event in or_event_list:
+							event__rewards.append((or_event, reward*gamma))
+			else:
+				pass
+			# Going through environment specific nodes
+			if len(non_reachable_env_node__rewards) > 0:
+				non_reachable_env_node, reward = non_reachable_env_node__rewards.pop()
+				possible_events = graph["node_pos_dependencies"][non_reachable_env_node]
+				event_found = False
+				event__rewards_appendage = []
+				for dep_obj_pos, possible_event in possible_events:
+					event__rewards_appendage.append((possible_event, reward))
+					if (inventory - rule_dict[possible_event[0]][1][possible_event[1]] >= 0):
+						nodes.append(dep_obj_pos)	
+						reward.append(reward)
+						event_found = True
+						break
+					else:
+						pass
+				if not event_found:
+					event__rewards += event__rewards_appendage
+			else:
+				pass
+		# Remove duplicates here, keep the ones with higher costs
+		final_nodes = []
+		final_rewards = []
+		for node, reward in zip(nodes, rewards):
+			if not node in final_nodes:
+				final_nodes.append(node)
+				final_rewards.append(reward)
+			else:
+				ind = final_nodes.index(node)
+				final_rewards[ind] = max(final_rewards[ind], reward)
+		return final_nodes, final_rewards
+
+
+	def get_next_options(self, node, use_graph_guide):
+		config = node.state_space
+		init_x, init_y = np.where(config == 1)
+		init_x, init_y = init_x[0], init_y[0]
+		_, options, costs = self.get_reachability_condition(config, (init_x, init_y))
+		combined_list = list(zip(options, costs))
+		np.random.shuffle(combined_list)
+		return zip(*combined_list)
+
+
+	def approximate_transition_step(self, node, option, use_graph_guide):
 		# Use rule dict here
-		return new_config
+		change_inventory = False
+		change_structure = False
+		obj = option[1]
+		obj_pos = option[0]
+		agent_pos = option[2]
+		new_state = node.state_space.copy()
+		new_inventory = node.inventory.copy()
+		new_graph = copy.deepcopy(node.graph)
+		num_rules = len(self.rule_dict[obj][0])
+
+		for i in range(num_rules):
+			transition = self.rule_dict[obj][0][i]
+			condition = self.rule_dict[obj][1][i]
+			
+			if (new_inventory - condition >= 0).all():
+				new_inventory += transition[:-1]
+				change_inventory = True
+			else:
+				pass
+
+			if transition[-1] == -1:
+				new_state[obj_pos[0], [obj_pos[1]]] = 0
+				import ipdb; ipdb.set_trace()
+				ind = new_graph["node_pos"][obj].index(obj_pos)
+				_ = new_graph["node_pos"][obj].pop(ind)
+				_ = new_graph["node_pos_dependencies"].pop(obj_pos, None)
+				for key in new_graph["node_pos_dependencies"].keys():
+					pop_this = False
+					for dep_obj_pos, _ in new_graph["node_pos_dependencies"][key]:
+						if dep_obj_pos == obj_pos:
+							pop_this = True
+						else:
+							pass
+					if pop_this: 
+						_ = new_graph["node_pos_dependencies"].pop(key, None)
+					else:
+						pass
+				change_structure = True
+				break
+			else:
+				pass
+
+		init_x, init_y = np.where(node.state_space == 1)
+		new_state[init_x[0], init_y[0]] = 0
+		new_state[agent_pos[0], agent_pos[1]] = 1
+
+		new_node = Node(new_inventory, new_state, new_graph)
+		new_node.reward_so_far = node.reward_so_far + np.multiply(new_inventory - node.inventory, bonus_inventory).sum()
+		new_node.skills_so_far = node.skills_so_far + [(option[0], option[1])]
+		new_node.cost_so_far = node.cost_so_far + cost
+				
+		return new_node, (change_inventory or change_structure)
 
 
-	def get_next_options(self, config, graph_guide=None):
-		options = None
-		return options
-
-
-	def play(self, initial_config, required_inventory, bonus_inventory, graph_guide=None):
-		solutions = {}
+	def play(self, initial_config, use_graph_guide=True):
+		# 
+		# alpha: independent to reusable/bonus event reward ratio
+		# beta: cost/reward ratio that we'll care for
+		# gamma: same as RL. discount factor (used to propagate rewards through the graph)
+		#
+		solutions = []
 		to_search = []
-		start_node = Node(np.zeros(required_inventory.shape), initial_config)
+		# Removing direction indicator
+		dir_x, dir_y = np.where(initial_config%1 == 0.5)
+		initial_config[dir_x, dir_y] += 0.5
+		# Configure start node
+		start_node = Node(np.zeros(self.required_inventory.shape), initial_config, self.graph_guide)
 		to_search.append(start_node)
+		# Graph search
+		count = 0
 		while len(to_search) > 0:
-			node = to_search.pop()
-			available_options = get_next_options(node.state_space, graph_guide)
-			for option in available_options:
-				inventory_change, new_options, cost = approximate_transition_step(node.state_space, option)
-				# reward = relation between different things
-				# add to search or otherwise
+			count += 1
+			node = to_search.pop(0)
+			available_options, costs = self.get_next_options(node, use_graph_guide)
+			# Running through possible options
+			for option, cost in zip(available_options, costs):
+				new_node, change = self.approximate_transition_step(node, option, use_graph_guide)
+				if not change:
+					continue
+				if (new_inventory - required_inventory >= 0).all():
+					indices_to_remove = []
+					pareto_front = True
+					for i, prev_sol in enumerate(solutions):
+						if (prev_sol.reward_so_far >= new_node.reward_so_far) and (prev_sol.cost_so_far <= new_node.cost_so_far):
+							pareto_front = False
+						elif (prev_sol.reward_so_far <= new_node.reward_so_far) and (prev_sol.cost_so_far >= new_node.cost_so_far):
+							indices_to_remove.append(i)
+					if pareto_front: solutions.append(new_node)
+					for i, ind in enumerate(indices_to_remove):
+						_ = solutions.pop(ind - i)
+				# Should we further search this node. As of now, we're just saying yes to everything
+				to_search.append(new_node)
+			#import ipdb; ipdb.set_trace()	
+			print(count, len(solutions), len(to_search))
+			if count > 1000:
+				import ipdb; ipdb.set_trace()	
 		return solutions
 
 
@@ -365,21 +534,24 @@ def main():
 	system3 = System3(system2.rule_dict)
 	# Load demos
 	for demo in [pickle.load(open("demos.pk", "rb"))[2]]:
-		# Let system 1, do the work
+		# System 1, segments original demo
 		demo_model = [ fullstate(s) for s in demo ]
 		for state in demo_model:
 			system1.next_state(state)
 		segmentation_index, skill_sequence = system1.result()
-		# Now system 2, update rules and get result
+		# System 2, update rules and get event/rule sequence
 		num_rules_prev = len(system2.rule_dict)
 		rule_sequence, reachability_set_sequence, event_position_sequence = system2.what_happened(skill_sequence, system1)
-		# We need to print graph here
-		required_inventory, bonus_inventory = system3.infer_objective(rule_sequence, reachability_set_sequence, event_position_sequence)
-		## 
-		#### To do: Update graph algorithm to include and use graph_guide
+		# System 3, infers objective, generates graph guide, and outputs skill sequence for the new environment
 		##
-		graph_guide = system3.get_dependency_graph_guide(system1.observation_function(demo_model[0]), independent_events, "event")
-		possible_skill_sequences = system3.play(initial_config, required_inventory, bonus_inventory, use_guide=False)
+		### To do ###
+		## 
+		# Here, we're using the same environment, we should experiment with multiple environments
+		#
+		objective = system3.infer_objective(rule_sequence, reachability_set_sequence, event_position_sequence)
+		graph_guide = system3.get_dependency_graph_guide(system1.observation_function(demo_model[0]))
+		possible_skill_sequences = system3.play(system1.observation_function(demo_model[0]))
+		self.reset_demo_objectives()
 	#print("Final set of rules: \n\n".format())
 	#for i, rule in enumerate(agent.rule_dict):
 	#		print("Rule Number:{} || obj:{}\nrules:{}\nconditions:{}\n\n".format(i, rule["object"], rule["rules"], rule["conditions"]))
