54113a65202803bad363c4d745e42ee6580db14a
vasisthn <nithin127vasisth@gmail.com>
2020-03-15 17:59:58 +0530

    Training is ongoing:

--- test_onedemoPERenv_cloning_skill.py
+++ test_onedemoPERenv_cloning_skill.py
@@ -1,4 +1,4 @@
-import pickle
+import os, pickle
 import numpy as np
 from system3 import *
 
@@ -13,26 +13,34 @@ import torch.optim as optim
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 3, 3)
-        self.conv2 = nn.Conv2d(3, 1, 3)
-        self.fc1 = nn.Linear(8 * 8 + 21, 32)
-        self.fc2 = nn.Linear(32, 10)
+        self.conv11 = nn.Conv2d(1, 3, 5)
+        self.conv12 = nn.Conv2d(3, 1, 5)
+        self.conv_shape1 = 4
+
+        #self.conv21 = nn.Conv2d(1, 3, 12)
+        #self.conv22 = nn.Conv2d(3, 1, 1)
+        #self.conv_shape2 = 1
+        
+        #self.fc1 = nn.Linear(self.conv_shape1 * self.conv_shape1 + self.conv_shape2 * self.conv_shape2 + 21, 48)
+        self.fc1 = nn.Linear(self.conv_shape1 * self.conv_shape1 + 21, 48)
+        self.fc2 = nn.Linear(48, 32)
+        self.fc3 = nn.Linear(32, 10)
         
     def forward(self, x1, x2):
-        x1 = F.relu(self.conv1(x1))
-        x1 = F.relu(self.conv2(x1))
-        x1 = x1.view(-1, 8 * 8)
-        x = torch.cat((x1, x2), 1)
+        x11 = F.relu(self.conv11(x1))
+        x11 = F.relu(self.conv12(x11))
+        #x12 = F.relu(self.conv21(x1))
+        #x12 = F.relu(self.conv22(x12))
+        #import ipdb; ipdb.set_trace()
+        x11 = x11.view(-1, self.conv_shape1 * self.conv_shape1)
+        #x12 = x12.view(-1, self.conv_shape2 * self.conv_shape2)
+        x = torch.cat((x11, x2), 1)
+        #x = torch.cat((torch.cat((x11, x12), 1), x2), 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
         return x
 
-
-
-# Single demonstration
-
-demos_water = pickle.load(open("demos_water_gold.pk", "rb"))
-demo = demos_water['1layer'][1]
 
 
 # Test environments
@@ -51,6 +59,10 @@ else:
 	pass
 
 
+
+#demos_water = pickle.load(open("demos_water_gold.pk", "rb"))
+#demo = demos_water['1layer'][1]
+
 #demo_model = [ fullstate(s) for s in demo ]
 #for state in demo_model:
 #	system1.next_state(state)
@@ -60,38 +72,44 @@ else:
 
 # Prepare dataset
 
-x1 = np.zeros((0,1,12,12))
-x2 = np.zeros((0,21))
-y = []
+if os.path.exists("skill_clone_dataset.pk"):
+	x1, x2, y = pickle.load(open("skill_clone_dataset.pk", "rb"))
+
+else:
+	x1 = np.zeros((0,1,12,12))
+	x2 = np.zeros((0,21))
+	y = []
 
 
-demo_type_strings = ["1layer", "2layer", "3layer", "gem_gold", "grass_gold", "iron_gold", "stone_gold", "water_gold", "wood_gold"]
-for demo_string in [demo_type_strings[0]]:
-	demos_rule_dict = pickle.load(open("demos_" + demo_string + ".pk", "rb"))
-	for i, demo in enumerate(demos_rule_dict['1layer']):
-		demo_model = [ fullstate(s) for s in demo ]
-		for state in demo_model:
-			system1.next_state(state)
-		segmentation_index, skill_sequence = system1.result()
-		system1.restart()
-		segmentation_index = [0] + segmentation_index
-		inventory = np.zeros(21)
-		for index, skill in zip(segmentation_index, skill_sequence):
-			curr_state = system1.observation_function(demo_model[index])
-			x1 = np.append(x1, np.expand_dims(np.expand_dims(curr_state, 0), 0), axis=0)
-			x2 = np.append(x2, np.expand_dims(inventory.copy(), 0), axis=0)
-			# Minus 3 is to adjust the classification categories
-			y.append(skill['object_before'] - 3)
-			# Update inventory as per rule base
-			if rule_base_access:
-				rule_tr = system2.rule_dict_oracle[skill['object_before']][0]
-				rule_pre = system2.rule_dict_oracle[skill['object_before']][1]
-				for tr, pre in zip(rule_tr, rule_pre):
-					if (inventory - pre >= 0).all():
-						inventory += tr[:-1]
-			else:
-				pass
-		
+	demo_type_strings = ["1layer", "2layer", "3layer", "gem_gold", "grass_gold", "iron_gold", "stone_gold", "water_gold", "wood_gold"]
+	for demo_string in demo_type_strings:
+		demos_rule_dict = pickle.load(open("demos_" + demo_string + ".pk", "rb"))
+		for i, demo in enumerate(demos_rule_dict['1layer']):
+			demo_model = [ fullstate(s) for s in demo ]
+			for state in demo_model:
+				system1.next_state(state)
+			segmentation_index, skill_sequence = system1.result()
+			system1.restart()
+			segmentation_index = [0] + segmentation_index
+			inventory = np.zeros(21)
+			for index, skill in zip(segmentation_index, skill_sequence):
+				curr_state = system1.observation_function(demo_model[index])
+				x1 = np.append(x1, np.expand_dims(np.expand_dims(curr_state, 0), 0), axis=0)
+				x2 = np.append(x2, np.expand_dims(inventory.copy(), 0), axis=0)
+				# Minus 3 is to adjust the classification categories
+				y.append(skill['object_before'] - 3)
+				# Update inventory as per rule base
+				if rule_base_access:
+					rule_tr = system2.rule_dict_oracle[skill['object_before']][0]
+					rule_pre = system2.rule_dict_oracle[skill['object_before']][1]
+					for tr, pre in zip(rule_tr, rule_pre):
+						if (inventory - pre >= 0).all():
+							inventory += tr[:-1]
+				else:
+					pass
+	
+	pickle.dump((x1, x2, y), open("skill_clone_dataset.pk", "wb"))
+
 
 # Prepare model
 net = Net()
@@ -103,10 +121,10 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 # Train
 
-for epoch in range(2):  # loop over the dataset multiple times
+for epoch in range(20):  
 
     running_loss = 0.0
-    for i in range(1000):
+    for i in range(10000): # loop over the dataset multiple times
         
         # zero the parameter gradients
         optimizer.zero_grad()
@@ -123,11 +141,13 @@ for epoch in range(2):  # loop over the dataset multiple times
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 20))
             running_loss = 0.0
+            #if np.isclose(running_loss, 0.0):
+            #	break
 
 print('Finished Training')
 
 
-
+import ipdb; ipdb.set_trace()
 
 
 		
